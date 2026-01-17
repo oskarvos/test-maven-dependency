681331ec33f4de5c4cfa1eb9ce40693e6f92404f
OLEG <oskaryahoo@gmail.com>
2026-01-17 13:36:18 +0300

    first commit

---
+++ business/src/main/java/com/oskarvos/Application.java
@@ -0,0 +1,12 @@
+package com.oskarvos;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application {
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+}
